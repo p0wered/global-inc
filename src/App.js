709077e605494c19b82cafb861173dddc39ef1a9
p0wered <powered.ui@gmail.com>
2024-04-09 21:32:12 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import titleImg from './global.png';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <section className='title-section' style={{backgroundImage: `url(${titleImg})`}}>
+            <div className='title-flexbox'>
+                <h1>Global Inc.</h1>
+                <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+                <h4>A amet aspernatur blanditiis exercitationem</h4>
+            </div>
+        </section>
+        <section className='section-2'>
+            <div className='container'>
+                <h3>Increase your revenue with an advanced fraud pipeline</h3>
+                <div className='info'>
+                    <div className='info-item'></div>
+                    <div className='info-item'></div>
+                    <div className='info-item'></div>
+                    <div className='info-item'></div>
+                </div>
+            </div>
+        </section>
+    </>
   );
 }
-
-export default App;
