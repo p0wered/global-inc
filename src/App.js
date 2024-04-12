75419e12d7d84b9ba17822077e4d436363917161
p0wered <powered.ui@gmail.com>
2024-04-13 00:10:44 +0300

    updated stats section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import titleImg from './global.png';
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import {useEffect} from "react";
+
+function InfoItem({title, desc, icon}){
+    return (
+        <div className='info-item'>
+            <i className={`bi bi-${icon}`}></i>
+            <h3>{title}</h3>
+            <p>{desc}</p>
+        </div>
+    )
+}
 
 export default function App() {
-  return (
+    useEffect(() => {
+        Aos.init();
+    }, []);
+    return (
     <>
         <section className='title-section' style={{backgroundImage: `url(${titleImg})`}}>
             <div className='title-flexbox'>
-                <h1>Global Inc.</h1>
-                <h4>Lorem ipsum dolor sit amet, consectetur</h4>
-                <h4>A amet aspernatur blanditiis exercitationem</h4>
+                <h1 data-aos='fade-right' data-aos-duration="500">Global Inc.</h1>
+                <h4 data-aos='fade-right' data-aos-duration="700">Lorem ipsum dolor sit amet, consectetur</h4>
+                <h4 data-aos='fade-right' data-aos-duration="700">A amet aspernatur blanditiis exercitationem</h4>
             </div>
         </section>
-        <section className='section-2'>
+        <section className='stats-section'>
             <div className='container'>
-                <h3>Increase your revenue with an advanced fraud pipeline</h3>
+                <h3 style={{paddingBottom: '3rem'}}>Increase your revenue with an advanced fraud pipeline</h3>
                 <div className='info'>
-                    <div className='info-item'></div>
-                    <div className='info-item'></div>
-                    <div className='info-item'></div>
-                    <div className='info-item'></div>
+                    <InfoItem title='98%' desc='Положительных отзывов' icon='heart'/>
+                    <InfoItem title='2000+' desc='Выполненных заказов' icon='box-seam'/>
+                    <InfoItem title='2000+' desc='Выполненных заказов' icon='box-seam'/>
+                    <InfoItem title='2000+' desc='Выполненных заказов' icon='box-seam'/>
                 </div>
             </div>
         </section>
     </>
-  );
-}
+    );
+    }
