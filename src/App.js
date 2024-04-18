2258437cc6f4c9d721262b188e29f2caecea4778
p0wered <powered.ui@gmail.com>
2024-04-18 19:54:14 +0300

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import titleImg from './dark_bg.jpg';
-import addressImg from './global.jpg'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import React, {useEffect} from "react";
-import {Parallax, ParallaxBanner, ParallaxProvider} from "react-scroll-parallax";
 
 function InfoItem({title, desc, icon}){
     return (
@@ -11,6 +8,16 @@ function InfoItem({title, desc, icon}){
             <i className={`bi bi-${icon}`}></i>
             <h3 style={{color: '#E85A4F'}}>{title}</h3>
             <p>{desc}</p>
+        </div>
+    )
+}
+
+function ContactInput({text, type}){
+    return (
+        <div className="form__group field">
+            <input type={type} className="form__field" placeholder={text} name={text} id={text}
+                   required/>
+            <label htmlFor={text} className="form__label">{text}</label>
         </div>
     )
 }
@@ -25,7 +32,7 @@ export default function App() {
                 <div className='title-flexbox'>
                     <h1 data-aos='fade-up' data-aos-duration="500">Global Inc.</h1>
                     <h3 data-aos='fade-up' data-aos-duration="650">Lorem ipsum dolor sit amet consectetur</h3>
-                    <a className="main-btn" data-aos='fade-up' data-aos-duration="700" href="#">CONTACT US</a>
+                    <a href="#" className="main-btn" data-aos='fade-up' data-aos-duration="700">CONTACT US</a>
                 </div>
             </div>
             <section className='stats-section'>
@@ -41,16 +48,22 @@ export default function App() {
             </section>
             <section className='address-section'>
                 <div className='container'>
-                    <h2 style={{marginBottom: '1rem'}}>About Us</h2>
+                    <h2 style={{marginBottom: '1rem'}}>About Company</h2>
                     <p style={{fontSize: 22}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aspernatur at culpa doloremque dolores ducimus eligendi enim eos error eveniet harum impedit libero magni molestiae nobis nostrum optio quae quia sequi temporibus ullam ut, veritatis voluptatibus. Corporis eligendi magnam pariatur quaerat, ratione rem reprehenderit ullam vero! Aperiam beatae consectetur consequuntur deleniti dignissimos ea, esse facilis, ipsa iure laudantium magnam molestiae nam nemo placeat possimus quam, soluta voluptate voluptatem. Adipisci aliquid animi eius enim esse exercitationem fugiat iste magnam molestias nam numquam odit omnis provident quaerat, sed similique sit, voluptas. Accusamus asperiores at eos harum nesciunt, praesentium quibusdam quo recusandae rem?</p>
                 </div>
             </section>
             <section>
                 <div className='container'>
-                    <div className='contact-box'>
+                    <form className='contact-box' id="contact-href">
                         <h2>Contact Us</h2>
-                        <input type="number"/>
-                    </div>
+                        <div className='field-merger'>
+                            <ContactInput text="Name" type='text'/>
+                            <ContactInput text="Surname" type='text'/>
+                        </div>
+                        <ContactInput text="Contact Number" type='tel'/>
+                        <ContactInput text="E-mail" type='email'/>
+                        <ContactInput text="Additional Info" type='text'/>
+                    </form>
                 </div>
             </section>
         </>
